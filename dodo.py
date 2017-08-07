677d375518050ed9547629849272eeac467b0e0a
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2017-08-07 12:52:57 +0200

    added a lot.  Including the unrotation script

--- dodo.py
+++ dodo.py
@@ -14,7 +14,6 @@ def read_motive_csv(fname):
                      index_col=[0, 1], tupleize_cols=True)
     df.index.names = ['Frame', 'Time']
     df.columns = [tuple(cols) if 'Unnamed' not in cols[3] else tuple([*cols[:-1] + (cols[-2],)]) for cols in df.columns]
-#     df.col
     df.columns = pd.MultiIndex.from_tuples(df.columns, names=['DataSource', 'ObjectName', 'CoordinateType', 'Axis'])
     return df
 
@@ -47,14 +46,17 @@ def convert_motive_csv_to_hdf5(csv_fname, h5_fname):
             f.write('Incomplete: {}, (csv: {} Frames of {} Motive Recorded Frames\r\n'.format(path.basename(csv_fname),
                 len(df), session_metadata['Total Exported Frames']))
 
-    nested_h5py.write_to_hdf5_group(h5_fname, df, '/', 
+    nested_h5py.write_to_hdf5_group(h5_fname, df, '/raw/', 
         compression='gzip', compression_opts=7)
 
     with h5py.File(h5_fname, 'r+') as f:
         f.attrs.update(session_metadata)
 
+    return None
 
-def add_orientation_dataset(h5_fname):
+
+def add_orientation_dataset(h5_fname, **kwargs):
+
     with h5py.File(h5_fname, 'r+') as f:
         f.copy('/raw', '/preprocessed')
         for name, obj in nested_h5py.walk_h5py_path(f['/preprocessed/']):
@@ -72,13 +74,11 @@ def add_orientation_dataset(h5_fname):
             f.create_dataset(name=obj.parent.name + '/Orientation',
                 data=odf.to_records(), compression='gzip', compression_opts=7)
 
-    with open(path.join(path.dirname(h5_fname), 'ori_added.txt'), 'w'):
-        pass
-
     return None
 
 
-def unrotate_objects(h5_fname, group='/preprocessed/Rigid Body', source_object_name='Arena', add_rotation=10.5, mean_center=True, index_cols=1):
+def unrotate_objects(h5_fname, group='/preprocessed/Rigid Body', source_object_name='Arena', 
+    add_rotation=10.5, mean_center=True, index_cols=1, y_offset=-0.66, **kwargs):
     """
     Un-rotate the objects in an hdf5 group by either a set rotationa mount, another object's rotation, or both.
 
@@ -91,6 +91,8 @@ def unrotate_objects(h5_fname, group='/preprocessed/Rigid Body', source_object_n
        
     """
     # Get rotation
+
+
     source_obj = nested_h5py.read_from_h5_group(h5_fname, path.join(group, source_object_name), index_cols=index_cols)
     mean_rot = source_obj.Rotation.mean()
     mean_rot.index = mean_rot.index.str.lower()
@@ -107,24 +109,30 @@ def unrotate_objects(h5_fname, group='/preprocessed/Rigid Body', source_object_n
     
     for body in body_paths:
         obj = nested_h5py.read_from_h5_group(h5_fname, path.join(group, body), index_cols=index_cols)
+        obj_pos = obj.Position.copy()
+        print('Mean Position before rotation', obj.mean())
         if mean_center:
-            obj.Position -= source_obj.Position.mean()
+            obj_pos.loc[:, ('X', 'Y', 'Z')] -= (source_obj.Position.X.mean(), -y_offset, source_obj.Position.Z.mean())
+
+        obj_pos.loc[:, ('X', 'Y', 'Z')] = obj_pos.loc[:, ('X', 'Y', 'Z')] @ rot_mat[:-1, :-1] @ manrot[:-1, :-1]
+        obj['Position'] = obj_pos
+        print('Mean Position before rotation', obj.mean())
+
         obj.Orientation @= rot_mat[:-1, :-1]
         obj.Orientation @= manrot[:-1, :-1]
         
         nested_h5py.write_to_hdf5_group(h5_fname, obj, body + '/',
                                         mode='r+', overwrite=True)
 
-    with open(path.join(path.dirname(h5_fname), 'unrotated.txt'), 'w'):
-        pass
-        
+    return None        
 
 
 event_log_dir = '/home/nickdg/theta_storage/data/VR_Experiments_Round_2/logs/event_logs/'
 settings_log_dir = '/home/nickdg/theta_storage/data/VR_Experiments_Round_2/logs/settings_logs/'
 
 
-def add_event_log(csv_fname, h5_fname):
+def add_event_log(csv_fname, h5_fname, **kwargs):
+
     log_fname = path.join(event_log_dir, path.basename(csv_fname))
 
     if not path.exists(log_fname):
@@ -159,6 +167,7 @@ def add_event_log(csv_fname, h5_fname):
     event_arguments = events.EventArguments.values.astype('S')
     del events['EventArguments']
     del events['DateTime']
+    
     with h5py.File(h5_fname, 'r+') as f:
         f.create_dataset('/events/eventlog', data=events.to_records(),
             compression='gzip', compression_opts=7)
@@ -167,13 +176,10 @@ def add_event_log(csv_fname, h5_fname):
         if len(phase_frames) > 0:
             f.create_dataset('/events/phaseStartFrameNum', data=phase_frames)
 
-    with open(path.join(path.dirname(h5_fname), 'event_log_added.txt'), 'w'):
-        pass
-
     return None
 
 
-def add_settings_log(json_fname, h5_fname):
+def add_settings_log(json_fname, h5_fname, **kwargs):
     """
     Writes a settings log to the hdf5 file as root user attributes, using csv data.
 
@@ -204,14 +210,40 @@ def add_settings_log(json_fname, h5_fname):
     for key, value in sess_data.items():
         if type(value) == bool:
             sess_data[key] = int(value)
+        if type(value) == type(None):
+            sess_data[key] = 0
 
     with h5py.File(h5_fname, 'r+') as f:
-        f.attrs.update(sess_data)
-
-    with open(path.join(path.dirname(h5_fname), 'settings_log_added.txt'), 'w'):
-        pass
+        try:
+            f.attrs.update(sess_data)
+        except TypeError as e:
+            import ipdb
+            ipdb.set_trace()
 
     return None
+
+
+def skip_if_conf_file_exists(conf_fname):
+    """
+    Skips running function if a given file exists. If not, runs functions and creates the file.
+
+    Arguments:
+      -conf_fname (str): filename to check for. (Note: Must be full path)
+    """
+    def decorator(fun):
+        def wrapper(*args, **kwargs):
+            
+            if path.exists(conf_fname):
+                print(fun.__name__ + 'already made for this file.  Skipping step...')
+                return
+            else:
+                output = fun(*args, **kwargs)
+                with open(conf_fname, 'w'):
+                    pass
+                return output
+        return wrapper
+    return decorator
+
 
 basedir = '/home/nickdg/theta_storage/data/VR_Experiments_Round_2/Converted Motive Files'
 
@@ -222,11 +254,17 @@ h5_fnames = [path.join(new_basedir, path.basename(path.splitext(name)[0]), path.
 
 
 def task_preprocess_all_data():
-    for csv_fname, h5_fname in zip(csv_fnames[:60], h5_fnames):
+    for csv_fname, h5_fname in zip(csv_fnames[:], h5_fnames):
+
+        dirname = path.dirname(h5_fname)
+
         if 'test' in csv_fname.lower():
             continue
         if 'habit' in csv_fname.lower():
             continue
+        # if not 'spat' in csv_fname.lower():
+        #     continue
+
         convert_task = {
             'actions': [(convert_motive_csv_to_hdf5, (csv_fname, h5_fname))],
             'targets': [h5_fname],
@@ -235,9 +273,11 @@ def task_preprocess_all_data():
         }
         yield convert_task
 
+        conf_fname = dirname + '/event_log_added.txt'
+        add_event_log_if_no_conf_file = skip_if_conf_file_exists(conf_fname)(add_event_log)
         event_task = {
-            'actions': [(add_event_log, (csv_fname, h5_fname,))],
-            # 'targets': [path.join(path.dirname(h5_fname), 'event_log_added.txt')],
+            'actions': [(add_event_log_if_no_conf_file, (csv_fname, h5_fname,))],
+            'targets': [conf_fname],
             'task_dep': [convert_task['name']],
             'file_dep': [h5_fname],
             'name': 'add_event_log: {}'.format(path.basename(h5_fname)),
@@ -245,38 +285,41 @@ def task_preprocess_all_data():
         }
         yield event_task
 
+        conf_fname = dirname + '/settings_log_added.txt'
+        add_settings_log_if_no_conf_file = skip_if_conf_file_exists(conf_fname)(add_settings_log)
         settings_task = {
-            'actions': [(add_settings_log, (csv_fname, h5_fname,))],
-            'targets': [path.join(path.dirname(h5_fname), 'settings_log_added.txt')],
+            'actions': [(add_settings_log_if_no_conf_file, (csv_fname, h5_fname,))],
+            'targets': [conf_fname],
             'task_dep': [event_task['name']],
             'file_dep': [h5_fname],
             'name': 'add_settings_log: {}'.format(path.basename(h5_fname)),
             'verbosity': 2,
-
         }
         yield settings_task
 
+        conf_fname = dirname + '/ori_added.txt'
+        add_orientation_dataset_if_no_conf_file = skip_if_conf_file_exists(conf_fname)(add_orientation_dataset)
+        ori_task = {
+            'actions': [(add_orientation_dataset_if_no_conf_file, (h5_fname,))],
+            'targets': [conf_fname],
+            # 'file_dep': [h5_fname],
+            'task_dep': [settings_task['name']],
+            'name': 'add_orientation: {}'.format(path.basename(h5_fname)),
+            'verbosity': 2,
+        }
+        yield ori_task
 
-        # ori_task = {
-        #     'actions': [(add_orientation_dataset, (h5_fname,))],
-        #     'targets': [path.join(path.dirname(h5_fname), 'ori_added.txt')],
-        #     'file_dep': [h5_fname],
-        #     'task_dep': [convert_task['name']],
-        #     'name': 'add_orientation: {}'.format(path.basename(h5_fname)),
-        # }
-        # yield ori_task
+        conf_fname = dirname + '/unrotated.txt'
+        unrotate_objects_if_no_conf_file = skip_if_conf_file_exists(conf_fname)(unrotate_objects)
+        rotate_task = {
+            'actions': [(unrotate_objects_if_no_conf_file, (h5_fname,))],
+            'targets': [conf_fname],
+            # 'file_dep': [h5_fname],
+            'task_dep': [ori_task['name']],
+            'name': 'unrotate: {}'.format(path.basename(h5_fname)),
+        }
+        yield rotate_task
 
-        # rotate_task = {
-        #     'actions': [(unrotate_objects, (h5_fname,))],
-        #     'targets': [path.join(path.dirname(h5_fname), 'unrotated.txt')],
-        #     'file_dep': [h5_fname],
-        #     'task_dep': [ori_task['name']],
-        #     'name': 'unrotate: {}'.format(path.basename(h5_fname)),
-        # }
-        # yield rotate_task
-
-
-    
 
 if __name__ == '__main__':
     import doit
